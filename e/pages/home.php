e6d4add887fe2bde21dc1a9144d6b71693ed9c80
Borbás Mátyás <25402691+borbasmatyas@users.noreply.github.com>
2023-06-30 14:42:07 +0200

    -

--- e/pages/home.php
+++ e/pages/home.php
@@ -12,6 +12,45 @@
 	</ul>
 </nav>
 
+<hr>
+
+<h1>Statisztikailag</h1>
+
+<h2>Aki a leghamarabb kezdte</h2>
+<table>
+	<?php
+
+		$query_stat = ('SELECT name FROM gyariszam.gyariszamok GROUP BY name');
+		$result_stat = $conn->query($query_stat);
+		while ($row_stat = $result_stat->fetch_assoc()) {
+			echo '<tr>';
+			echo '<td>' . $row_stat['name'] . '</td>';
+			echo '</tr>';
+		}
+
+	?>
+
+</table>
+
+
+
+<h2>Aki a legtöbbet</h2>
+<table>
+	<?php
+
+		$query_stat = ('SELECT name, COUNT(name) AS mennyi FROM gyariszam.gyariszamok GROUP BY name ORDER BY COUNT(name) DESC');
+		$result_stat = $conn->query($query_stat);
+		while ($row_stat = $result_stat->fetch_assoc()) {
+			echo '<tr>';
+			echo '<td>' . $row_stat['name'] . ' </td>';
+			echo '<td>' . $row_stat['mennyi'] . ' pitty</td>';
+			echo '</tr>';
+		}
+
+	?>
+
+</table>
+
 
 
 
